c67cd6d0fa146275ea476bd2bd69a43f649c9c2c
Gehad Mohammad <95745981+GehadMohammed@users.noreply.github.com>
2022-05-28 16:24:30 +0200

    Update ec2

--- ec2.tf
+++ ec2.tf
@@ -53,35 +53,6 @@ resource "aws_instance" "bastion-01" {
     EOF
 }
 
-resource "aws_instance" "bastion-02" {
- instance_type = "t2.micro"
- ami = "ami-004b73b0102ad2eee" # (Ubuntu)
- subnet_id = module.network.public-subnet-02-id
- security_groups = [aws_security_group.public-security-group.id]
- key_name = aws_key_pair.key-pair.key_name
- disable_api_termination = false
- ebs_optimized = false
- root_block_device {
- volume_size = "100"
- }
- tags = {
- "Name" = "${var.name}-bastion-02-G"
- }
-
-   provisioner "local-exec" {
-    command = "echo bastion-02 ${self.public_ip} >> public_ips.txt"
-  }
-
-   depends_on = [ module.network.vpc-01-id, module.network.igw-id, aws_db_instance.rds-gehad ]
-
-     user_data = <<EOF
-    #!/bin/sh
-    sudo apt-get update
-    sudo apt-get install -y mysql-client
-    echo ${aws_db_instance.rds-gehad.address} >/tmp/dbdomain.txt
-    sudo mv /tmp/dbdomain.txt /dbdomain.txt
-    EOF
-}
 
 #EC2-application
 
@@ -102,18 +73,4 @@ resource "aws_instance" "application-01" {
 }
 
 
-resource "aws_instance" "application-02" {
- instance_type = "t2.micro"
- ami = "ami-004b73b0102ad2eee" # (Ubuntu)
- subnet_id = module.network.private-subnet-02-id
- security_groups = [aws_security_group.private-security-group.id]
- key_name = aws_key_pair.key-pair.key_name
- disable_api_termination = false
- ebs_optimized = false
- root_block_device {
- volume_size = "100"
- }
- tags = {
- "Name" = "${var.name}-application-02-G"
- }
-}
+
